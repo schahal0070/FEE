09e3b5d9d445e8820bc0b88a7494eac4688e30bb
Shrishti Chahal <schahal0070@gmail.com>
2026-09-01 15:36:41 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   g4fee/Vite-project/.gitignore
    	new file:   g4fee/Vite-project/README.md
    	new file:   g4fee/Vite-project/eslint.config.js
    	new file:   g4fee/Vite-project/index.html
    	new file:   g4fee/Vite-project/package.json
    	new file:   g4fee/Vite-project/public/favicon.svg
    	new file:   g4fee/Vite-project/public/icons.svg
    	new file:   g4fee/Vite-project/src/App.css
    	new file:   g4fee/Vite-project/src/App.jsx
    	new file:   g4fee/Vite-project/src/assets/hero.png
    	new file:   g4fee/Vite-project/src/assets/react.svg
    	new file:   g4fee/Vite-project/src/assets/vite.svg
    	new file:   g4fee/Vite-project/src/index.css
    	new file:   g4fee/Vite-project/src/main.jsx
    	new file:   g4fee/Vite-project/vite.config.js
    	modified:   g4fee/src/App.jsx
    	new file:   g4fee/src/ShowList.jsx
    	new file:   g4fee/src/Store.jsx
    	modified:   g4fee/src/index.css
    	modified:   g4fee/src/main.jsx

--- g4fee/Vite-project/src/main.jsx
+++ g4fee/Vite-project/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Store from './Store.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Store />
+    <App />
   </StrictMode>,
 )
