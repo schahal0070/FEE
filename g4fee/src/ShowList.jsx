a760aef5b282235d8bd249413342e6e363ee1828
Shrishti Chahal <schahal0070@gmail.com>
2026-09-03 15:09:08 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   g4fee/src/ShowList.jsx
    	new file:   g4fee/src/ShowList2.jsx
    	modified:   g4fee/src/index.css
    	modified:   g4fee/src/main.jsx

--- g4fee/src/ShowList.jsx
+++ g4fee/src/ShowList.jsx
@@ -1,1 +1,38 @@
-function
+import React, {useEffect,useState} from "react";
+
+function ShowList(props) 
+{
+    const [products,setProducts]=useState([]);
+
+    useEffect(() => 
+    {
+        let data = localStorage.getItem("products");
+        data=JSON.parse(data);
+        setProducts(data);
+        if(props.brand) 
+        {
+            setProducts(data.filter((item)=>item.brand === props.brand));
+        }
+    }, []); // dependency array ie empty array
+
+    return (
+        <section className="fyc">
+            <div className="b1" style={{padding: "1rem"}}>
+                <h3>Product list {props.brand}</h3>
+                <div className="bg1">
+                    <span style={{width:"3rem"}}>ID</span>
+                    <span style={{width:"7rem"}}>Brand</span>
+                    <span style={{width:"7rem"}}>Price</span>
+                </div>
+                {products.map((item)=>(
+                    <div key={item.ID}>
+                        <span style={{width:"3rem"}}>{item.id}</span>
+                        <span style={{width:"3rem"}}>{item.brand}</span>
+                        <span style={{width:"3rem"}}>{item.price}</span>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
+export default ShowList;
--- g4fee/src/main.jsx
+++ g4fee/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Store from './Store.jsx'
+import ShowList from './ShowList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Store />
+    <Store brand = {"Pepe"} />
+    <ShowList brand ={"Pepe"} />
   </StrictMode>,
 )
